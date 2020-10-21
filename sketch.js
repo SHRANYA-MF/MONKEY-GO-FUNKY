8d2c2ba878dfd2ca38d80b09037541cb5ab042d4
SHRANYA-MF <73236185+SHRANYA-MF@users.noreply.github.com>
2020-10-21 21:28:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+
+var monkey , monkey_running
+var banana ,bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var score
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstaceImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  
+
+  
+}
+
+
+function draw() {
+
+  
+}
+
+
+
+
+
+
